222ad51e3c1137ad8693f940f675b8c3678c2b7e
Jordan Evans <jevans17@uoguelph.ca>
2021-07-07 15:57:44 -0400

    done v1

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import feedparser
 import sqlite3
 import yfinance as yf
+import sys
+
 
 ARTICLES = []
 TRANSACTIONS = []
@@ -32,12 +34,28 @@ class Article:
         return self.action + ": " + ",".join(self.tickers)
     
 
-def article_exists(a_id):
-    for art in ARTICLES:
-        if art.a_id == a_id:
-            return True
-    
-    return False
+def get_article_ids():
+    con = sqlite3.connect('database.db')
+    cur = con.cursor()
+
+    ids = []
+
+    for row in cur.execute("SELECT * FROM articles"):
+        ids.append(row[1])
+
+    con.close()
+
+    return ids
+
+
+def add_article(art):
+    con = sqlite3.connect('database.db')
+
+    cur = con.cursor()
+    cur.execute(f"INSERT INTO articles (a_id, action, date_published, date_str, tickers) VALUES ('{art.a_id}','{art.action}','{art.date_published}','{art.date_str}','{' '.join(art.tickers)}')")
+
+    con.commit()
+    con.close()
 
 
 def create_article(entry):
@@ -125,39 +143,53 @@ def complete_transactions(article):
     return transaction_list
 
 
-def update_transactions(transactions):
+def add_transactions(transactions):
     con = sqlite3.connect('database.db')
 
     cur = con.cursor()
 
     # Create table
-    cur.execute('''CREATE TABLE stocks
-                (date text, trans text, symbol text, qty real, price real)''')
-
-    # Insert a row of data
-    cur.execute("INSERT INTO stocks VALUES ('2006-01-05','BUY','RHAT',100,35.14)")
+    for t in transactions:
+        cur.execute(f"INSERT INTO transactions (action, action_time, market, ticker, quantity, price) VALUES ('{t.action}','{t.action_time}','{t.market}','{t.ticker}',{t.quantity},{t.price})")
 
     con.commit()
     con.close()
 
 
+def get_transactions():
+    con = sqlite3.connect('database.db')
+    cur = con.cursor()
+
+    trans = []
+
+    # Create table
+    for row in cur.execute(f"SELECT * FROM transactions"):
+        trans.append(Transaction(row[0], row[1], row[2], row[3], row[4], row[5], row[6]))
+
+    con.close()
+
+    return trans
+
+
 def main():
     NewsFeed = feedparser.parse("https://www.fool.ca/feed/")
 
+    # Get ID list
+    article_ids = get_article_ids()
+
     for e in NewsFeed.entries:
-        if not article_exists(e['id']):
+        if e['id'] not in article_ids:
             art = create_article(e)
-            ARTICLES.append(art)
+            
             if art == None:
                 print("Error creating article.")
             else:
                 print(art)
+                add_article(art)
 
             if art.action != "HOLD":
                 result = complete_transactions(art)
-            
-                for r in result:
-                    TRANSACTIONS.append(r)
+                add_transactions(result)
         else:
             print("Article exists already.")
     
@@ -165,6 +197,83 @@ def main():
         print(tr)
             
 
+def view_portfolio():
+    trans = get_transactions()
+
+    cash = 0.0
+    holdings = {}
+    markets = {}
+
+    for t in trans:
+
+        if t.action == "BUY":
+            if t.ticker in holdings:
+                # Total cost base
+                new_cost = ((t.price * t.quantity) + (holdings[t.ticker]['price'] * holdings[t.ticker]['quantity'])) / (t.quantity + holdings[t.ticker]['quantity'])
+                # new_cost = (t.price + holdings[t.ticker]['price']) / (t.quantity + holdings[t.ticker]['quantity'])
+                # print(f"{t.price} + {holdings[t.ticker]['price']} / {t.quantity} + {holdings[t.ticker]['quantity']}")
+
+                holdings[t.ticker]['quantity'] += t.quantity
+                holdings[t.ticker]['price'] = new_cost
+            else:
+                holdings[t.ticker] = {
+                    'quantity': t.quantity,
+                    'price': t.price
+                }
+        elif t.action == "SELL":
+            if t.ticker in holdings:
+                if holdings[t.ticker][quantity] > 0:
+                    q_sold = 0
+                    holdings[t.ticker]['quantity'] -= t.quantity
+                    if holdings[t.ticker]['quantity'] < 0:
+                        q_sold = t.quantity - holdings[t.ticker]['quantity']
+                    else:
+                        q_sold = t.quantity
+                    
+                    cash += q_sold * holdings[t.ticker]['price']
+                    holdings[t.ticker]['quantity'] = 0
+            # else:
+            #     holdings[t.ticker] = {
+            #         'quantity': t.quantity,
+            #         'price': t.price
+            #     }
+
+    for t in holdings:
+        tik = yf.Ticker(t)
+        # print(tik.info.keys())
+        if "currentPrice" in tik.info:
+            cur_price = tik.info['currentPrice']
+            percent = ((cur_price - holdings[t]['price']) / holdings[t]['price']) * 100.0
+            percent_str = f"({round(percent, 2)})" if percent < 0 else f"{round(percent, 2)}"
+
+            price_str = "%.2f" % round(holdings[t]['price'], 2)
+            cur_price_str = "%.2f" % round(cur_price, 2)
+
+            holdings[t]['cur_price'] = cur_price
+            
+            print(f"{t}: {holdings[t]['quantity']} x ${price_str} -> {cur_price} %{percent_str}")
+        else:
+            print(f"Error getting current price of {t}.")
+            holdings[t]['cur_price'] = 0.0
+            holdings[t]['quantity'] = 0.0
+            holdings[t]['price'] = 0.0
+            # print(tik.info)
+    
+    book_total = 0.0
+    cur_total = 0.0
+    for t in holdings:
+        book_total += (holdings[t]['quantity'] * holdings[t]['price'] )
+        cur_total += (holdings[t]['quantity'] * holdings[t]['cur_price'] )
+    
+    pct_gain = ((cur_total - book_total) / book_total) * 100
+    pct_gain_str = f"({round(pct_gain, 2)})" if pct_gain < 0 else f"{round(pct_gain, 2)}"
+
+    print(f"# Portfolio Value: ${cur_total}  %{pct_gain_str}")
+
+
 
 if __name__ == '__main__':
-    main()
+    if len(sys.argv) > 1 and sys.argv[1] == "view":
+        view_portfolio()
+    else:
+        main()
